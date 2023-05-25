fb3db4c9bbbc1f13533ad11b9023815aefbef0e1
David Brandon <davidpbrandon@gmail.com>
2023-05-25 10:20:51 +0100

    USP section additions and styles

--- template-parts/usp-section.php
+++ template-parts/usp-section.php
@@ -9,11 +9,11 @@
       <div class="col-md">
         <div class="card">
           <div class="card-header" data-toggle="collapse" data-target="#collapse1">
-            Column 1
-          </div>
+  
+        </div>
           <div id="collapse1" class="collapse show">
             <div class="card-body">
-              Content for column 1
+              <span class="text-hightlight">WordPress/Woocoommerce Support:</span> 
             </div>
           </div>
         </div>
@@ -21,11 +21,11 @@
       <div class="col-md">
         <div class="card">
           <div class="card-header" data-toggle="collapse" data-target="#collapse2">
-            Column 2
-          </div>
+  
+        </div>
           <div id="collapse2" class="collapse show">
             <div class="card-body">
-              Content for column 2
+            <span class="text-hightlight">Site Auditing & Optimisation:</span>
             </div>
           </div>
         </div>
@@ -33,11 +33,11 @@
       <div class="col-md">
         <div class="card">
           <div class="card-header" data-toggle="collapse" data-target="#collapse3">
-            Column 3
-          </div>
+ 
+        </div>
           <div id="collapse3" class="collapse show">
             <div class="card-body">
-              Content for column 3
+            <span class="text-hightlight">Content Generation:</span>
             </div>
           </div>
         </div>
@@ -53,16 +53,16 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Step One - Reach out!</h5>
-            <p class="card-text">Get in touch using our contact form, and let us know what we can help you with.</p>
+          <h5 class="usp-title">Step One - Reach out!</h5>
+            <p class="card-text">Get in touch using our contact form, and <span class="text-highlight">let us know what we can help you with</span>.</p>
           </div>
         </div>
       </div>
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Step 2 - Figuring it out</h5>
-            <p class="card-text">We'll take a look a get back to you with a no-obligation quote (we might require some access to your site's code to do this.)</p>
+          <h5 class="usp-title">Step 2 - Figuring it out</h5>
+            <p class="card-text">We'll take a look a get back to you with a <span class="text-highlight">no-obligation quote</span> (we might require some access to your site's code to do this.)</p>
           </div>
         </div>
       </div>
@@ -71,16 +71,16 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Step 3 - Agreeing the work</h5>
-            <p class="card-text">If you're happy to proceed we'll ask for a 50% deposit in advance, with the balance due on satisfactory completion of the agreed work. Simple!</p>
+          <h5 class="usp-title">Step 3 - Agreeing the work</h5>
+          <p class="card-text">If you're happy to proceed we'll ask for a <span class="text-highlight">50% deposit in advance</span>, with the balance due on satisfactory completion of the agreed work. Simple!</p>
           </div>
         </div>
       </div>
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Step 4 - Checking in</h5>
-            <p class="card-text">We'll schedule a check-in with you to make sure everything is still working as expected, and to see if there's anything else we might be able to assist with.</p>
+            <h5 class="usp-title">Step 4 - Checking in</h5>
+            <p class="card-text"><span class="text-highlight">We'll schedule a check-in with you</span> to make sure everything is still working as expected, and to see if there's anything else we might be able to assist with.</p>
           </div>
         </div>
       </div>
