b752e36fea4229d2c1d8bfd881f18708ac4db713
David Brandon <davidbrandon@davids-mbp.home>
2023-06-30 19:02:05 +0100

    USP text and icons

--- template-parts/usp-section.php
+++ template-parts/usp-section.php
@@ -17,33 +17,33 @@
           </div>
         </div>  
       </div>
-    </div>
+    </div> 
     <div class="col-md">
       <div class="card">
-        <div class="card-header" data-toggle="collapse" data-target="#collapse2">
+        <div class="card-header" data-toggle="collapse" data-target="#collapse1">
           <img class="usp-img" src="/wp-content/uploads/2023/05/wc-and-wp.png">
         </div>
-        <div id="collapse2" class="collapse show">
+        <div id="collapse1" class="collapse show">
           <div class="card-body">
-            <span class="text-hightlight">Support, Site Auditing & Optimisation:</span>
+            <p class="card-text card-title">Site support & optimisation</p> <p>Represent your brand and/or e-commerce service with a <span class="text-highlight"> professional digital presence.</p>
           </div>
-        </div>
+        </div>  
       </div>
-    </div>
+    </div> 
     <div class="col-md">
       <div class="card">
-        <div class="card-header" data-toggle="collapse" data-target="#collapse3">
+        <div class="card-header" data-toggle="collapse" data-target="#collapse1">
           <img class="usp-img" src="/wp-content/uploads/2023/05/wc-and-wp.png">
         </div>
-        <div id="collapse3" class="collapse show">
+        <div id="collapse1" class="collapse show">
           <div class="card-body">
-            <span class="text-hightlight">Content Generation:</span>
+            <p class="card-text card-title">SEO & copywriting</p> <p>Represent your brand and/or e-commerce service with a <span class="text-highlight"> professional digital presence.</p>
           </div>
-        </div>
+        </div>  
       </div>
-    </div>
-  </div>
-</div>
+    </div> 
+
+
 <!-- USP grid layout -->
 <div class="container" data-aos="fade-up">
   <div class="container usp text-center">
@@ -51,7 +51,7 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-          <h5 class="usp-title">Reach Out!</h5>
+          <h5 class="usp-title"><i class="fa-solid fa-bullhorn"></i> Reach Out!</h5>
             <p class="card-text">Get in touch using our contact form, and <span class="text-highlight"><a href="#contact-form-anchor"> let us know what we can help you with</a></span>.</p>
           </div>
         </div>
@@ -59,7 +59,7 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-          <h5 class="usp-title">Services</h5>
+          <h5 class="usp-title"><i class="fa-solid fa-trophy"></i> Expertise</h5>
             <p class="card-text">Be it a full site build or technical assistance, we'll get back to you with a <span class="text-highlight">no-obligation quote</span> (we might require some access to your site's code if you have a technical issue).</p>
           </div>
         </div> 
@@ -69,7 +69,7 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-          <h5 class="usp-title">Moving Forward</h5>
+          <h5 class="usp-title"><i class="fa-solid fa-forward"></i> Moving Forward</h5>
           <p class="card-text">If you're happy to proceed with the word we've quoted you for, then we'll ask for a <span class="text-highlight">50% deposit in advance</span>, with the balance due on satisfactory completion of the agreed work. Simple!</p>
           </div>
         </div>
@@ -77,7 +77,7 @@
       <div class="col-sm-6 col-md-6 col-lg-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="usp-title">SEO & Content</h5>
+            <h5 class="usp-title"><i class="fa-solid fa-ranking-star"></i> SEO & Content</h5>
             <p class="card-text">We also offer SEO services such as <span class="text-highlight">speed optimisation</span>, copywriting and <span class="text-highlight">optimisation for Google SERPs</span>, to help put you and your brand on the map.
           </div>
         </div>
