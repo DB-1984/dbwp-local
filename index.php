0a076ecf4cff37b115ccda69044fa2812ad39a35
David Brandon <davidbrandon@davids-mbp.home>
2023-07-11 18:42:27 +0100

    Styling/changing blog index layout

--- index.php
+++ index.php
@@ -1,52 +1,47 @@
 <?php get_header(); ?>  
 <body <?php body_class(); ?>>
-<div class="row blog-index">
-    <h1 class="blog-title">The Blog</h1>
-    <p class="blog-intro">Tips, tricks, and articles about all things <span class="text-highlight">Wordpress/WooCommerce</span>.</p>
-    <?php 
-    $count = 0;
-    if ( have_posts() ) : while ( have_posts() ) : the_post();
-        if ($count % 4 === 0) {
-            echo '<div class="row">';
-        }
-        ?>
-        <div class="col-md-3">
-            <div class="card mb-4">
-                <div class="card-body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <?php if (has_post_thumbnail()): ?>
-                                    <div class="featured-image blog-index">
-                                        <?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
-                                    </div>
-                                <?php endif; ?>
+    <div class="container">
+        <div class="row blog-index">
+        <h1 class="blog-title">The Blog</h1>
+        <p class="blog-intro">Tips, tricks, and articles about all things <span class="text-highlight" style="color: #fdbb0a">WordPress/WooCommerce</span>.</p>
+        </div>
+        <div class="row">
+            <?php 
+            $count = 0;
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+                if ($count % 3 === 0) {
+                    echo '<div class="row mx-auto">';
+                }
+                ?>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <?php if (has_post_thumbnail()): ?>
+                            <div class="featured-image blog-index">
+                                <?php the_post_thumbnail('large', array('class' => 'card-img-top img-fluid')); ?>
                             </div>
+                        <?php endif; ?>
+                        <div class="card-body">
+                            <h2 class="card-title"><?php the_title(); ?></h2>
+                            <p class="card-text"><?php echo wp_trim_words( get_the_excerpt(), 20, '...' ); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary"><p>Read More</p></a>
                         </div>
                     </div>
-                    <h2 class="card-title"><?php the_title(); ?></h2>
-                    <p class="card-text"><?php the_excerpt(); ?></p>
-                    <a href="<?php the_permalink(); ?>" class="btn btn-primary"><p>Read More</p></a>
                 </div>
-            </div>
+                <?php
+                $count++;
+                if ($count % 3 === 0) {
+                    echo '</div>';
+                }
+            endwhile; 
+            if ($count % 3 !== 0) :
+                echo '</div>';
+            endif;
+            else :
+                echo '<div class="col-md-12">
+                    <p>' . esc_html__( 'Sorry, no posts were found.', 'textdomain' ) . '</p>
+                </div>';
+            endif;
+            ?>
         </div>
-        <?php
-        $count++;
-        if ($count % 4 === 0) {
-            echo '</div>';
-        }
-    endwhile; 
-    if ($count % 4 !== 0) {
-        echo '</div>';
-    }
-    else {
-        echo '<div class="col-md-12">
-            <p>' . esc_html__( 'Sorry, no posts were found.', 'textdomain' ) . '</p>
-        </div>';
-    }
-   
-    endif;
-    ?>
-</div>
-
+    </div>
 <?php get_footer(); ?>
